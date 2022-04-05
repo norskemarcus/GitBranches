e1d6ded776d2d885a2ebf7918822e0f48f23e040
norskemarcus <marc791j@stud.kea.dk>
2022-04-05 14:40:10 +0200

    added souts in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,24 @@ public class Main {
     public static void main(String[] args) {
 
         Cat soya = new Cat("Soya");
+        Cat nusse = new Cat ("Nusse");
         Dog topsy = new Dog("Topsy");
+        Dog balder = new Dog("Balder");
 
-        System.out.println(soya.name);
+        System.out.print(soya.name);
         soya.makeSound();
         soya.eat("fish");
         soya.showMood(true);
 
+        System.out.print(nusse.name);
+        nusse.showMood(false);
 
         System.out.println(topsy.name);
         topsy.makeSound();
         topsy.eat("dogfood");
         topsy.showMood(false);
+
+        System.out.println(balder.name);
+        balder.showMood(true);
     }
 }
