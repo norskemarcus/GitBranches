8761b055deee8eef53329e1c4cf54c61bbf40977
norskemarcus <marc791j@stud.kea.dk>
2022-04-05 09:17:34 +0200

    made changes in main (cat and dog says something)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        cat.makeSound();
+        dog.makeSound();
     }
 }
